705993d7e10529a42b42db0cb553dd06837587f0
Chirayu Krishnappa <chirayu@chirayuk.com>
2015-05-08 15:45:34 -0700

    parse_html.es6 → parse_html.ts

---
+++ lib/parse_html.ts
@@ -0,0 +1,28 @@
+///<reference path='../typings/node/node.d.ts'/>
+import * as assert from 'assert';
+
+// TODO: Get type definitions for parse5 and convert this to an import statement.
+const parse5 = require('parse5');
+
+const _ADAPTER = parse5.TreeAdapters.default;
+
+export const adapter = _ADAPTER;
+
+// Returns the root element.
+export function parseHtml(html: string) {
+  let parser = new parse5.Parser(_ADAPTER);
+  let root = parser.parse(html);
+  // Normally, "root" is the document node and contains the optional doctype
+  // node and the HTML node as children.  We'll skip the doctype node and
+  // return the only HTML node.
+  let element = null;
+  for (let child of _ADAPTER.getChildNodes(root)) {
+    if (!_ADAPTER.isDocumentTypeNode(child)) {
+      if (element !== null) {
+        throw Error("Found more than one element at the root level while parsing HTML text");
+      }
+      element = child;
+    }
+  }
+  return element;
+}
